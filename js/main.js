9be5f5aab3943d1ce1966834500bc04e079ad315
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-02-20 17:26:01 -0500

    Create click event for each square on board and function that keeps track of whether that click is odd or even (x or o)

--- js/main.js
+++ js/main.js
@@ -1,19 +1,18 @@
 $(document).ready(function() {
-  console.log("Document is ready!")
 
-  $('.switch').on('click', function () {
-    console.log("Click registered.");
+  var count = 0;
 
-    if ($('body').hasClass('light')) {
-      $('.status').text('Hey, who turned off the lights?');
-      $('body.light').toggleClass('dark');
-      $('button.on').toggleClass('off');
+  function isEven(n) {
+    return n % 2 == 0;
+  }
+
+  $('td').on('click', function() {
+    count++;
+    if (isEven(count)) {
+      console.log("This is an O.");
     } else {
-      $('.status').text("It's so bright in here!");
-      $('body.dark').toggleClass('light');
-      $('button.off').toggleClass('on');
+      console.log("This is an X.");
     };
-
   });
 
 });
