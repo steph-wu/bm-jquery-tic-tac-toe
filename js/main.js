ae82160ccc6b1196fe8bbda6b1295badc64ec466
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-02-20 18:07:39 -0500

    Add x or o to square on click

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
   var count = 0;
+  var marker = document.createElement('div');
 
   function isEven(n) {
     return n % 2 == 0;
@@ -10,8 +11,10 @@ $(document).ready(function() {
     count++;
     if (isEven(count)) {
       console.log("This is an O.");
+      $(this).append("<div class=marker>o</div>");
     } else {
       console.log("This is an X.");
+      $(this).append("<div class=marker>x</div>");
     };
   });
 
