dd147289610ecc97f7da7cd570a1f3b4d2b65e5b
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-02-20 18:14:08 -0500

    Prevent square from being clicked twice

--- js/main.js
+++ js/main.js
@@ -1,20 +1,25 @@
 $(document).ready(function() {
 
   var count = 0;
-  var marker = document.createElement('div');
 
   function isEven(n) {
     return n % 2 == 0;
   }
 
   $('td').on('click', function() {
-    count++;
-    if (isEven(count)) {
-      console.log("This is an O.");
-      $(this).append("<div class=marker>o</div>");
+
+    if ($(this).hasClass('marked')) {
+      alert("This box has already been marked!");
     } else {
-      console.log("This is an X.");
-      $(this).append("<div class=marker>x</div>");
+      $(this).addClass('marked')
+      count++;
+      if (isEven(count)) {
+        console.log("This is an O.");
+        $(this).append("<div class=marker>o</div>");
+      } else {
+        console.log("This is an X.");
+        $(this).append("<div class=marker>x</div>");
+      };
     };
   });
 
