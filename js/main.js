ba3f6cd96599f37263e078863aebf2afb1e06b21
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-02-20 19:03:33 -0500

    Create checkWin function that evaluates if any rows, columns or diagonals contain 3 of the same mark

--- js/main.js
+++ js/main.js
@@ -6,6 +6,39 @@ $(document).ready(function() {
     return n % 2 == 0;
   }
 
+  function checkWin() {
+
+    if (
+      ( $('.1').hasClass('has-x') && $('.2').hasClass('has-x') && $('.3').hasClass('has-x') ) ||
+      ( $('.4').hasClass('has-x') && $('.5').hasClass('has-x') && $('.6').hasClass('has-x') ) ||
+      ( $('.7').hasClass('has-x') && $('.8').hasClass('has-x') && $('.9').hasClass('has-x') ) ||
+      ( $('.1').hasClass('has-x') && $('.4').hasClass('has-x') && $('.7').hasClass('has-x') ) ||
+      ( $('.2').hasClass('has-x') && $('.5').hasClass('has-x') && $('.8').hasClass('has-x') ) ||
+      ( $('.3').hasClass('has-x') && $('.6').hasClass('has-x') && $('.9').hasClass('has-x') ) ||
+      ( $('.1').hasClass('has-x') && $('.5').hasClass('has-x') && $('.9').hasClass('has-x') ) ||
+      ( $('.3').hasClass('has-x') && $('.5').hasClass('has-x') && $('.7').hasClass('has-x') )
+    ) {
+      alert("X wins!")
+    };
+
+    if (
+      ( $('.1').hasClass('has-o') && $('.2').hasClass('has-o') && $('.3').hasClass('has-o') ) ||
+      ( $('.4').hasClass('has-o') && $('.5').hasClass('has-o') && $('.6').hasClass('has-o') ) ||
+      ( $('.7').hasClass('has-o') && $('.8').hasClass('has-o') && $('.9').hasClass('has-o') ) ||
+      ( $('.1').hasClass('has-o') && $('.4').hasClass('has-o') && $('.7').hasClass('has-o') ) ||
+      ( $('.2').hasClass('has-o') && $('.5').hasClass('has-o') && $('.8').hasClass('has-o') ) ||
+      ( $('.3').hasClass('has-o') && $('.6').hasClass('has-o') && $('.9').hasClass('has-o') ) ||
+      ( $('.1').hasClass('has-o') && $('.5').hasClass('has-o') && $('.9').hasClass('has-o') ) ||
+      ( $('.3').hasClass('has-o') && $('.5').hasClass('has-o') && $('.7').hasClass('has-o') )
+    ) {
+      alert("O wins!")
+    };
+    
+    console.log("checkWin function called");
+  }
+
+  // 123, 456, 789, 147, 258, 369, 159, 357 (combinations)
+
   $('td').on('click', function() {
 
     if ($(this).hasClass('marked')) {
@@ -15,10 +48,14 @@ $(document).ready(function() {
       count++;
       if (isEven(count)) {
         console.log("This is an O.");
+        $(this).addClass('has-o');
         $(this).append("<div class=marker>o</div>");
+        checkWin();
       } else {
         console.log("This is an X.");
+        $(this).addClass('has-x');
         $(this).append("<div class=marker>x</div>");
+        checkWin();
       };
     };
   });
